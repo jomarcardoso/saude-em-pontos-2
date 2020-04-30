ff3ac5d46fd1bfb3cf0080cd4c7d95561b5ee6e9
Jomar <jomar.cardsoso@cwi.com.br>
2020-04-29 23:56:22 -0300

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,16 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import Quiz from '../components/quiz';
 import { Link } from "gatsby-theme-material-ui";
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Layout from '../components/layout';
 import Advertise from '../components/advertise';
-// import * as serviceWorker from '../serviceWorker';
 import AccountContext from '../components/account-context';
 
 function Index() {
   const { account, setAccount } = useContext(AccountContext);
   const [readAdvertise, setReadAdvertise] = useState(false);
   const rendered = typeof window !== 'undefined';
-
-  // useEffect(() => {
-  //   if (rendered) {
-  //     serviceWorker.register();
-  //   }
-  // }, [rendered]);
 
   if (rendered && !account) {
     if (!readAdvertise) {
--- src/pages/meal.js
+++ src/pages/meal.js
@@ -5,7 +5,10 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 function useFood() {
   const data = useStaticQuery(graphql`
@@ -14,7 +17,8 @@ function useFood() {
         childDbJson {
           foods {
             name,
-            id
+            id,
+            image
           }
         }
       }
@@ -24,8 +28,17 @@ function useFood() {
   return data.file.childDbJson.foods;
 }
 
+const useStyles = makeStyles({
+  selectIcon: {
+    minWidth: '20px',
+    width: '20px',
+    marginRight: '10px'
+  }
+});
+
 export default function Meal({ data }) {
   const foods = useFood();
+  const classes = useStyles();
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -33,15 +46,19 @@ export default function Meal({ data }) {
 
   return (
     <Layout currentPage="meal" pageName="Cadastrar refeição">
-      <img src="images/fruits/banana.svg" alt=""/>
       <form action="/" method="post" onSubmit={handleSubmit}>
-        <Grid container spacing={3}>
+        <Grid container spacing={5}>
           <Grid item xs={12}>
             <InputLabel id="food">Alimento</InputLabel>
             <Select labelId="food" id="select" value={0}>
               <MenuItem value={0} />
               {foods.map((food) => (
-                <MenuItem value={food.id}>{food.name}</MenuItem>
+                <MenuItem value={food.id}>
+                  <ListItemIcon className={classes.selectIcon}>
+                    <img src={food.image} alt="" />
+                  </ListItemIcon>
+                  <ListItemText primary={food.name} />
+                </MenuItem>
               ))}
             </Select>
           </Grid>
