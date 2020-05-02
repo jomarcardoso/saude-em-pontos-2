701b048a286ac5896f55564c68065df205f253e7
Jomar <jomar.cardsoso@cwi.com.br>
2020-05-02 01:52:03 -0300

    travis

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,10 +3,7 @@ const path = require('path');
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
   const foodPage = path.resolve(`src/pages/food.tsx`);
-  // Query for markdown nodes to use in creating pages.
-  // You can query for whatever data you want to create pages for e.g.
-  // products, portfolio items, landing pages, etc.
-  // Variables can be added as the second function parameter
+
   return graphql(
     `
       query {
@@ -30,10 +27,8 @@ exports.createPages = ({ graphql, actions }) => {
       throw result.errors;
     }
 
-    // Create blog post pages.
     result.data.file.childDbJson.foods.forEach((edge) => {
       createPage({
-        // Path for this page — required
         path: `food/${edge.enName}`,
         component: foodPage,
         context: edge,
